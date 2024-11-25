3ce6c2331d9cfeb7f3aae3e5133105534da2f41a
GladisDM <gladysmarr123@gmail.com>
2024-11-25 14:27:51 -0500

    Mi primer commit

---
+++ src/main/java/ec/espoch/edu/ec/miprimerrproyecto/Miprimerrproyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.espoch.edu.ec.miprimerrproyecto;
+
+/**
+ *
+ * @author USER
+ */
+public class Miprimerrproyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
